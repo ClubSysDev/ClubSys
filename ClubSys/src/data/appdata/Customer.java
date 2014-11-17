fcc011741056f6db84826e036a90edb8713ee2a5
DAT14V1-CasperVejdiksen <casp539b@stud.kea.dk>
2014-11-17 11:44:15 +0100

    Customer

--- ClubSys/src/data/appdata/Customer.java
+++ ClubSys/src/data/appdata/Customer.java
@@ -1,5 +1,89 @@
 package data.appdata;
 
-public class Customer {
+public class Customer 
+{
+
+	private String name;
+	private int CVR;
+	private String Address;
+	private String email;
+	private int phone;
+	private String password;
+
+
+	public void Customer(String name, int CVR, String address, String email, int phone, String password) 
+	{
+		this.name = name;
+		this.CVR = CVR;
+		this.Address = address;
+		this.email = email;
+		this.phone = phone;
+		this.password = Encrypter.encrypt(password);
+	}
 	
+	public void Customer()
+	{
+		
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getCVR()
+	{
+		return CVR;
+	}
+
+	public void setCVR(int cVR)
+	{
+		CVR = cVR;
+	}
+
+	public String getAddress()
+	{
+		return Address;
+	}
+
+	public void setAddress(String address)
+	{
+		Address = address;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+
+	public int getPhone()
+	{
+		return phone;
+	}
+
+	public void setPhone(int phone)
+	{
+		this.phone = phone;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = Encrypter.encrypt(password);
+	}
+
 }
