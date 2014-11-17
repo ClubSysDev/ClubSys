7d6513ba08dd7219cf77e12025ce86a600d8b8b3
Kenneth Jespersen <kennethjespersen@Kenneths-MacBook-Pro.local>
2014-11-17 11:49:11 +0100

    hallo

---
+++ ClubSys/src/app/CustomerController.java
@@ -0,0 +1,19 @@
+package app;
+
+import gui.KundeListeGui;
+import gui.KundeOversigtGui;
+import gui.RedigerKundeGui;
+
+public class CustomerController
+{
+
+	private RedigerKundeGui redigerGui; 
+	private KundeListeGui listeGui;
+	private KundeOversigtGui oversigtGui;
+	
+	public void newRedigerGui()
+	{
+		
+	}
+	
+}
---
+++ ClubSys/src/data/database/CustomerDB.java
@@ -0,0 +1,13 @@
+package data.database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerDB
+{
+
+	public List<Customer> getCustomer(List list)
+	{
+		
+	}
+}
